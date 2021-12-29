9e7cb81a76e15309419e3de80a27605d0e042718
Anwesh43 <anweshthecool0@gmail.com>
2021-12-29 15:23:51 +0530

    adding SpringBarButton component

---
+++ src/SpringBarButton.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { useStyle } from './hooks'
+
+interface SBBProps {
+    w : number,
+    h : number, 
+    scale : number, 
+    onClick : Function 
+}
+const SpringBarButton = (props : SBBProps) => {
+    const {squareStyle, circleStyle} = useStyle(props.w, props.h, props.scale)
+    return (
+        <React.Fragment>
+            <div style = {squareStyle()} onClick = {() => props.onClick()}></div>
+            <div style = {circleStyle()} onClick = {() => props.onClick()}></div>
+        </React.Fragment>
+    )
+}
+
+export default SpringBarButton 
